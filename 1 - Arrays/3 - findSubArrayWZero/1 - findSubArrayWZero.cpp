8c2477a9af5479973c40d0e47012aeb699907ad9
Jay Kim <j.legendary2@gmail.com>
2017-11-06 17:30:35 -0500

    main

---
+++ 1 - Arrays/3 - findSubArrayWZero/1 - findSubArrayWZero.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+
+void printSubArr(int arr[], int n);
+
+int main()
+{
+	int arr[] = {3,4,-7,3,1,3,1,-4,-2,-2};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	printSubArr(arr, n);
+	return 0;
+}
+
+void printSubArr(int arr[], int n)
+{
+	
+}
