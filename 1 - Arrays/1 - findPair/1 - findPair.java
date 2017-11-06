1dc39d11e1c1654769f42f79cd96c970fd0bb51d
Jay Kim <j.legendary2@gmail.com>
2017-11-06 12:35:30 -0500

    Added main function

--- 1 - Arrays/1 - findPair/1 - findPair.java
+++ 1 - Arrays/1 - findPair/1 - findPair.java
@@ -23,6 +23,14 @@ class FindPair
 		System.out.println("Pair not found");
 	}	
 
+	public static void main(String[] args)
+	{
+		int arr[] = {8,7,2,5,3,1};
+		int sum = 10;
+
+		findPair(arr, sum);
+	}
+
 }
 
 
