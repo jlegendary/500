a2bdeb90dbe593c56647eac43e7dc0f0f6dfba7e
Jay Kim <j.legendary2@gmail.com>
2017-11-06 13:44:22 -0500

    Changed to C++ to use STL's sort

--- 1 - Arrays/1 - findPair/2 - findPair.c
+++ 1 - Arrays/1 - findPair/2 - findPair.c
@@ -1,4 +1,9 @@
+// Notes link: http://www.geeksforgeeks.org/c-qsort-vs-c-sort/
+//   Will use cpp for STL's sort, rather than C's qsort.
+
+
 #include <stdio.h>
+#include <stdlib.h>
 
 void findPair(int arr[], int n, int sum);
 
@@ -12,4 +17,22 @@ int main()
 	findPair(arr, n, sum);
 
 	return 0;
+}
+
+void findPair(int arr[], int n, int sum)
+{
+	std::sort(arr, arr+n);
+	int low = 0;
+	int high = n-1;
+
+	while (low<high)
+	{
+		if (arr[low] + arr[high] == sum)
+		{
+			std::cout << "Pair found" << endl;
+		}
+		(arr[low] + arr[high] < sum)? low++: high--;
+	}
+
+	std::cout << "Pair not found" << endl;
 }
