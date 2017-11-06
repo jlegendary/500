320d22667b0ff142d4be344d47894d094aaea5f8
Jay Kim <j.legendary2@gmail.com>
2017-11-06 12:25:12 -0500

    Debugged array

--- 1 - Arrays/1 - findPair/1 - findPair.c
+++ 1 - Arrays/1 - findPair/1 - findPair.c
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
-void findPair(int, int, int);
+void findPair(int arr[], int n, int sum);
 
 int main ()
 {
-   int arr = {8,7,2,5,3,1}
+   int arr[] = {8,7,2,5,3,1};
    int sum = 10;
    // divisor of sizeof(arr[0] is the size of the zeroth element of the array
    // Source: https://stackoverflow.com/questions/37538/how-do-i-determine-the-size-of-my-array-in-c
@@ -26,7 +26,9 @@ void findPair(int arr[], int n, int sum)
          if (arr[i]+arr[j]==sum)
          {
             printf("Pair found at index %d and %d", i, j);
+            printf("\n");
          }
       }
    }
 }
+
