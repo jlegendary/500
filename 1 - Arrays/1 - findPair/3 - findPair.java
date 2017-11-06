8750cf9fe1de0d68793c190c40b3f0f9ef4ca8c9
Jay Kim <j.legendary2@gmail.com>
2017-11-06 15:30:03 -0500

    Added

--- 1 - Arrays/1 - findPair/3 - findPair.java
+++ 1 - Arrays/1 - findPair/3 - findPair.java
@@ -6,13 +6,20 @@ class FindPair
 	public static void findPair(int arr[], int sum)
 	{
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-
+		for (int i=0; i<arr.length; i++)
+		{
+			if(map.containsKey(sum-arr[i]))
+			{
+				System.out.println("Pair found at index " + map.get(sum-arr[i]) + " and " + i);
+			}
+			map.put(arr[i], i);
+		}
+		System.out.println("Pair not found");
 	}
 	public static void main (Static[] args)
 	{
 		int arr[] = {8,7,2,5,3,1};
-		int sum = 0;
+		int sum = 10;
 		findPair(arr, sum);
 	}
 }
