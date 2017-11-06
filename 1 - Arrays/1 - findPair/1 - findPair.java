9fbda96a7ab69d3f67675d38a677708b53d162c2
Jay Kim <j.legendary2@gmail.com>
2017-11-06 12:33:10 -0500

    Fix - minor issue at line 18

--- 1 - Arrays/1 - findPair/1 - findPair.java
+++ 1 - Arrays/1 - findPair/1 - findPair.java
@@ -4,15 +4,18 @@ class FindPair
 {
 	public static void findPair(int arr[], int sum)
 	{
+		// Length of array
 		int n = arr.length;
-
+		// All element except last
 		for (int i=0; i<n-1; i++)
 		{
+			// Start i-th until end
 			for (int j=i+1; j<n; j++)
 			{
+				// If sum is found
 				if (arr[i] + arr[j] == sum)
 				{
-					System.out.println("Pair found at index" + i + " and " + j);
+					System.out.println("Pair found at index" + i + " and " + j + "\n");
 				}
 			}
 		}
