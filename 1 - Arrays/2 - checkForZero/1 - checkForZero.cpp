e8a3b1ba4a03346e31843eb214459df74c5a73b4
Jay Kim <j.legendary2@gmail.com>
2017-11-06 17:00:45 -0500

    Debugged 1 - checkForZero

--- 1 - Arrays/2 - checkForZero/1 - checkForZero.cpp
+++ 1 - Arrays/2 - checkForZero/1 - checkForZero.cpp
@@ -8,12 +8,29 @@ int main()
 	int arr[] = {4, 2, -3, -1, 0, 4};
 	int n = sizeof(arr)/sizeof(arr[0]);
 
-	checkForZeroSum(arr, n) : 
+	checkForZeroSum(arr, n) ?
 	std::cout << "Arry exists" << arr[n] << std::endl : 
 	std::cout << "Array don't exist" << std::endl;
+
+	return 0;
 }
 
 bool checkForZeroSum(int arr[], int n)
 {
-	
+	// Empty set to store sum of elements of each sub-array
+	std::unordered_set<int> set;
+
+	// Insert 0 first
+	set.insert(0);
+
+	int sum=0;
+	for(int i=0; i<n; i++)
+	{
+		sum+=arr[i];
+		if(set.find(sum)!= set.end())
+			return true;
+		else
+			set.insert(sum);
+	}
+	return false;
 }
